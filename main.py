24ecb16e875062a055e61aa5de55b44a1fa3ce1a
4Bits-RIF <mohammed.arif.log@gmail.com>
2024-10-12 22:07:15 +0530

    Prototype Login and Signup System Done.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
+import json
 from flask import *
 from tinydb import *
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 db = TinyDB(os.path.join("static", "Database", "db.json"))
 query = Query()
@@ -19,6 +21,12 @@ def login():
 @app.route("/signup")
 def signup():
 	return render_template("Signup.html")
+
+@app.route("/profile")
+def profile():
+	if session.get("userID"):
+		return render_template("Profile.html", profileData=[session["userName"], session["userID"], session["userEmail"]])
+	return redirect("index")
 
 @app.route("/b-signup", methods=["POST"])
 def b_signup():
@@ -39,8 +47,15 @@ def b_login():
 		userID = request.form["userID"]
 		userPassword = request.form["userPassword"]
 
-		if len(usersTable.search(query.userID == userID) and usersTable.search(query.userPassword == userPassword)) > 0:
-			print("Logged In!")
+		userData = usersTable.search(query.userID == userID) and usersTable.search(query.userPassword == userPassword)
+
+		if len(userData) > 0:
+			
+			session["userName"] = userData[0]["userName"]
+			session["userID"] = userData[0]["userID"]
+			session["userEmail"] = userData[0]["userEmail"]
+
+			return redirect(url_for("profile"))
 		else:
 			print("Incorrect Info!")
 	return redirect(url_for("index"))
