0221f2ddebec1b57bdc7419759e17af7ee6c35bb
4Bits-RIF <mohammed.arif.log@gmail.com>
2024-10-13 03:36:23 +0530

    Started Build the Profile Page and Blogging Functionalities. Fixed Some issues with Login.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Running The Web App:
 
-	1. Install Python 3 	-> 		https://python.org/downloads/
-	2. Install Flask 		-> 		pip install Flask
-	3. Install TinyDB		-> 		pip install tinydb
-	4. Clone the Repo		-> 		https://github.com/MSDandekar/PJ.git
-	
-	5. Run main.py
-	6. Go to http://127.0.0.1:4444/
+    1. Install Python 3     ->      https://python.org/downloads/
+    2. Install Flask        ->      pip install Flask
+    3. Install TinyDB       ->      pip install tinydb
+    4. Clone the Repo       ->      https://github.com/MSDandekar/PJ.git
+    
+    5. Run main.py
+    6. Go to http://127.0.0.1:4444/
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import json
+import datetime
 from flask import *
 from tinydb import *
 
@@ -7,8 +7,10 @@ app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 db = TinyDB(os.path.join("static", "Database", "db.json"))
-query = Query()
+cursor = Query()
+
 usersTable = db.table("Users")
+blogsTable = db.table("Blogs")
 
 @app.route("/")
 def index():
@@ -25,8 +27,14 @@ def signup():
 @app.route("/profile")
 def profile():
 	if session.get("userID"):
-		return render_template("Profile.html", profileData=[session["userName"], session["userID"], session["userEmail"]])
-	return redirect("index")
+		return render_template("Profile.html", profileData={"userName": session["userName"], "userID": session["userID"], "userEmail": session["userEmail"]})
+	return redirect(url_for("index"))
+
+@app.route("/blogs")
+def blogs():
+	if session.get("userID"):
+		return render_template("Blogs.html", blogs=blogsTable.all())
+	return redirect(url_for("index"))
 
 @app.route("/b-signup", methods=["POST"])
 def b_signup():
@@ -47,7 +55,12 @@ def b_login():
 		userID = request.form["userID"]
 		userPassword = request.form["userPassword"]
 
-		userData = usersTable.search(query.userID == userID) and usersTable.search(query.userPassword == userPassword)
+		print(userID)
+		print(userPassword)
+
+		userData = usersTable.search((cursor.userID == userID) & (cursor.userPassword == userPassword))
+
+		print(userData)
 
 		if len(userData) > 0:
 			
@@ -59,6 +72,26 @@ def b_login():
 		else:
 			print("Incorrect Info!")
 	return redirect(url_for("index"))
+
+@app.route("/b-logout", methods=["POST"])
+def b_logout():
+	if request.method == "POST":
+		session.pop("userName", None)
+		session.pop("userID", None)
+		session.pop("userEmail", None)
+	return redirect(url_for("index"))
+
+@app.route("/b-blog", methods=["POST"])
+def b_blog():
+	if request.method == "POST":
+		blogTitle = request.form["blogTitle"]
+		blog = request.form["blog"]
+
+		blogsTable.insert({"userID": session["userID"], "dateTime": str(datetime.datetime.now()), "blogTitle": blogTitle, "blog": blog})
+
+		return redirect(url_for("blogs"))
+
+	return redirect(url_for("profile"))
 
 if __name__ == "__main__":
 	app.run(debug=True, port="4444")
